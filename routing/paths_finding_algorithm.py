9667b1aac0819cb3b551b158c0331a1888badf3c
r0masik <vorob97@yandex.ru>
2019-05-06 19:00:52 +0300

    Improve paths finding algorithm New classes Router, Graph

--- routing/paths_finding_algorithm.py
+++ routing/paths_finding_algorithm.py
@@ -4,60 +4,59 @@
 class PathsFindingAlgorithm:
     def __init__(self, graph, n_nodes):
         self.graph = graph
-        self.vertices = self.graph.keys()
         self.nodes_in_path = n_nodes
-        self.paths = {}
+        self.paths_n_nodes = {}
 
-    def find_paths(self, v_from, v_to):
-        self.paths = {}
+    def find_optimal_path(self, v_from, v_to):
+        self.paths_n_nodes = {}
         init_params = {
             'path': v_from,
             'length': 0,
             'depth': 1
         }
         self.search(v_from, v_to, init_params)
-        return self.paths
+        optimal_path = min(self.paths_n_nodes, key=self.paths_n_nodes.get)
+        return {optimal_path: self.paths_n_nodes[optimal_path]}
 
     def search(self, v_cur, v_to, params):
         if v_cur == v_to:
             if params['depth'] == self.nodes_in_path:
-                self.paths[params['path']] = params['length']
+                self.paths_n_nodes[params['path']] = params['length']
             return
 
         if params['depth'] < self.nodes_in_path:
-            v_next_list = [v for v in self.vertices if self.graph[v_cur][v]]
+            v_next_list = [v for v in self.graph[v_cur]]
             for v_next in v_next_list:
                 if v_next not in params['path']:
+                    v_next_network = list(self.graph[v_cur][v_next].keys())[0]
                     v_next_params = {
-                        'path': params['path'] + v_next,
-                        'length': params['length'] + self.graph[v_cur][v_next],
+                        'path': params['path'] + f';{v_next_network};{v_next}',
+                        'length': params['length'] + self.graph[v_cur][v_next][v_next_network],
                         'depth': params['depth'] + 1
                     }
                     self.search(v_next, v_to, v_next_params)
 
 
 if __name__ == '__main__':
-    v_list = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'K']
-    list_e_list = [
-        [None, 2, 2, 3, None, None, None, None, None],
-        [2, None, None, None, 3, None, None, None, None],
-        [2, None, None, 1, None, 4, None, None, None],
-        [3, None, 1, None, 4, 4, 5, None, None],
-        [None, 3, None, 4, None, 3, None, 2, None],
-        [None, None, 4, 4, 3, None, None, 3, None],
-        [None, None, None, 5, None, None, None, 3, 2],
-        [None, None, None, None, 2, 3, 3, None, 2],
-        [None, None, None, None, None, None, 2, 2, None]
-    ]
-    my_graph = dict(zip(v_list, [dict(zip(v_list, e_list)) for e_list in list_e_list]))
-    for vi in my_graph:
-        for vj in my_graph:
-            print_str = f'{vj} -> {vi}: {my_graph[vi][vj]}'
-            if my_graph[vi][vj] is not None:
-                print_str += '\t'
-            print(print_str, end='\t')
+
+    my_graph = {'A': {'B': {'local network': 2}, 'C': {'local network': 2}, 'D': {'local network': 3}},
+                'B': {'A': {'local network': 2}, 'E': {'local network': 3}},
+                'C': {'A': {'local network': 2}, 'D': {'local network': 1}, 'F': {'local network': 4}},
+                'D': {'A': {'local network': 3}, 'C': {'local network': 1}, 'E': {'local network': 4},
+                      'F': {'local network': 4}, 'G': {'local network': 5}},
+                'E': {'B': {'local network': 3}, 'D': {'local network': 4}, 'F': {'local network': 3},
+                      'H': {'local network': 2}},
+                'F': {'C': {'local network': 4}, 'D': {'local network': 4}, 'E': {'local network': 3},
+                      'H': {'local network': 3}},
+                'G': {'D': {'local network': 5}, 'H': {'local network': 3}, 'K': {'local network': 2}},
+                'H': {'E': {'local network': 2}, 'F': {'local network': 3}, 'G': {'local network': 3},
+                      'K': {'local network': 2}},
+                'K': {'G': {'local network': 2}, 'H': {'local network': 2}}}
+    for vi, v_next in my_graph.items():
+        for vj in v_next:
+            print(f'{vi} -> {vj}: {my_graph[vi][vj]}', end='\t')
         print()
 
     alg = PathsFindingAlgorithm(my_graph, 5)
-    for path, length in alg.find_paths('A', 'K').items():
-        print(f'{path} = {length}')
+    res = alg.find_optimal_path('A', 'K')
+    print(res)
---
+++ routing/router.py
@@ -0,0 +1,108 @@
+"""Router"""
+
+import random
+from routing.paths_finding_algorithm import PathsFindingAlgorithm
+
+
+class Graph:
+    def __init__(self):
+        self.normalized = None
+        self._graph_edges = None
+        self._graph = None
+        self.update_graph()
+
+    def update_graph(self):
+        self.normalized = False
+        self._graph_edges = self.network_graph()
+
+    def network_graph(self):
+        return [
+            # from, to, network, weight
+            ['A', 'B', 'net1', 2],
+            ['A', 'C', 'net1', 2],
+            ['A', 'D', 'net1', 3],
+            ['B', 'E', 'net1', 3],
+            ['C', 'D', 'net1', 1],
+            ['C', 'F', 'net1', 4],
+            ['D', 'E', 'net1', 4],
+            ['D', 'F', 'net1', 4],
+            ['D', 'G', 'net1', 5],
+            ['E', 'F', 'net1', 3],
+            ['E', 'H', 'net1', 2],
+            ['F', 'H', 'net1', 3],
+            ['G', 'H', 'net1', 3],
+            ['G', 'K', 'net1', 2],
+            ['H', 'K', 'net1', 2],
+
+            # add some multiple edges
+            ['A', 'D', 'net2', 2],
+            ['D', 'F', 'net2', 2],
+            ['F', 'H', 'net2', 2],
+        ]
+
+    def modify_with_random(self):
+        if not self.normalized:
+            self.normalize()
+        for vi in self._graph:
+            for vj in self._graph[vi]:
+                for net in self._graph[vi][vj]:
+                    self._graph[vi][vj][net] += random.uniform(-2, 2)
+
+    def merge_multiple_edges(self):
+        if not self.normalized:
+            self.normalize()
+        for vi in self._graph:
+            for vj in self._graph[vi]:
+                optimal_net = min(self._graph[vi][vj], key=self._graph[vi][vj].get)
+                self._graph[vi][vj] = {optimal_net: self._graph[vi][vj][optimal_net]}
+
+    def normalize(self):
+        self._graph = {}
+        for edge in self._graph_edges:
+            self._normalize_edge(edge[0], edge[1], edge[2], edge[3])
+            self._normalize_edge(edge[1], edge[0], edge[2], edge[3])
+        self.normalized = True
+
+    def _normalize_edge(self, v_from, v_to, net, weight):
+        if v_from not in self._graph:
+            self._graph[v_from] = {}
+        if v_to not in self._graph[v_from]:
+            self._graph[v_from][v_to] = {}
+        self._graph[v_from][v_to][net] = weight
+
+    @property
+    def graph(self):
+        if not self.normalized:
+            self.normalize()
+        return self._graph
+
+
+class Router:
+    def __init__(self):
+        self.routes = {}
+
+    def new_route(self, client_id):
+        graph = Graph()
+        graph.modify_with_random()
+        graph.merge_multiple_edges()
+
+        alg = PathsFindingAlgorithm(graph.graph, 5)
+        route = alg.find_optimal_path('A', 'K')
+        print(alg.paths_n_nodes)
+        print(route)
+
+        self.routes[client_id] = route
+
+        # подготовка транзитных узлов
+
+
+def print_graph(graph):
+    for vi, v_next in graph.items():
+        for vj in v_next:
+            print(f'{vi} -> {vj}: {graph[vi][vj]}', end='\t')
+        print()
+
+
+if __name__ == '__main__':
+    r = Router()
+    r.new_route('A')
